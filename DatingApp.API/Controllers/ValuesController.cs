daa094ed3889224044e0b710d715152553b89b5f
longhai2210 <36153109+longhai2210@users.noreply.github.com>
2019-03-24 14:59:49 +0700

    Register

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,7 +19,7 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
